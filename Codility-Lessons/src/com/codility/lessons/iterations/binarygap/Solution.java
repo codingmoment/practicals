49fe4e99cecb854eab8d32200b90372e93d1e676
Nilesh Wani <nileshwaani@gmail.com>
2021-04-18 17:03:20 +0530

    Solution for https://app.codility.com/programmers/lessons/1-iterations/binary_gap/

---
+++ Codility-Lessons/src/com/codility/lessons/iterations/binarygap/Solution.java
@@ -0,0 +1,60 @@
+package com.codility.lessons.iterations.binarygap;
+
+/**
+ * Solution for Codility lesson: https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
+ */
+public class Solution {
+
+  public static void main(String[] args) {
+    Solution solution = new Solution();
+    solution.solution(2147483647);
+  }
+
+  /**
+   * Lesson solution
+   */
+  public int solution(int N) {
+    // Get the binary representation for given number
+    int[] binaryArray = decimal2Binary(N);
+
+    int maxBinaryGap = 0, binaryGap = 0;
+    boolean gapStarted = false;
+
+    // Traverse the binary array to find the maximum sequence of 0s
+    for (int digit : binaryArray) {
+      // We start counting 0s only after first 1 is found
+      if (!gapStarted) {
+        if (digit == 1) {
+          gapStarted = true;
+        }
+      }
+      else {
+        if (digit == 0) {
+          binaryGap++;
+        }
+        else {
+          // We have come across 1
+          // We store the maximum occurrence in maxBinaryGap and reset the binaryGap to 0
+          maxBinaryGap = Math.max(maxBinaryGap, binaryGap);
+          binaryGap = 0;
+        }
+      }
+    }
+
+    return maxBinaryGap;
+  }
+
+  /**
+   * Method to convert a decimal number into a binary representation
+   */
+  private int[] decimal2Binary(int decimal) {
+    int k = (int) (Math.log(decimal) / Math.log(2)) + 1;
+    int[] binaryArray = new int[k];
+
+    while (decimal > 0) {
+      binaryArray[--k] = decimal % 2;
+      decimal /= 2;
+    }
+    return binaryArray;
+  }
+}
