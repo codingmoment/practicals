1d8d73c0eb0c676e5e2b653fa6a0b1d61f4a5f23
Nilesh Wani <nileshwaani@gmail.com>
2021-05-04 23:37:33 +0530

    React complete guide - Expense tracker

---
+++ React/react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import "./Expenses.css";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filterYear}
+          onFilterChange={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
