35ac822e41bcc3cd089973e8f2cf8db090bf1805
Nilesh Wani <nileshwaani@gmail.com>
2021-05-06 16:46:21 +0530

    React complete guide - User management

---
+++ React/user-management/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsers((prevUsers) => {
+      return [
+        ...prevUsers,
+        {
+          id: Math.random().toString(),
+          name: uName,
+          age: uAge,
+        },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={users} />
+    </div>
+  );
+}
+
+export default App;
