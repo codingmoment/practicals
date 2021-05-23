f90944e39f03e8792222c29dd6e9c213ce8661e2
Nilesh Wani <nileshwaani@gmail.com>
2021-05-23 19:24:40 +0530

    React complete guide - Food order app - managing cart and modal state

--- React/food-order-app/src/components/Layout/Header.js
+++ React/food-order-app/src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>MummyMeal</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="Delicious Food!" />
