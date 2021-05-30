eed0ab8a3eff2bc26301282abbfb47768a0f62bd
Nilesh Wani <nileshwaani@gmail.com>
2021-05-30 16:54:13 +0530

    React complete guide - Food order app - added visual effect to bump the cart button

--- React/food-order-app/src/components/Layout/HeaderCartButton.js
+++ React/food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,41 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import CartIcon from "./CartIcon";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighligthted, setButtonIsHighligthted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItems = cartCtx.items.reduce((lastAmount, item) => {
     return lastAmount + item.amount;
   }, 0);
 
+  const buttonClasses = `${classes.button} ${
+    buttonIsHighligthted ? classes.bump : ""
+  }`;
+
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setButtonIsHighligthted(true);
+
+    const timer = setTimeout(() => {
+      setButtonIsHighligthted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
