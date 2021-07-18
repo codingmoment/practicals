8bd8bcf265a5ae3719719b38a5be9955b2873237
Nilesh Wani <nileshwaani@gmail.com>
2021-07-18 16:57:33 +0530

    Solution for https://app.codility.com/programmers/lessons/6-sorting/distinct/

---
+++ Codility-Lessons/src/com/codility/lessons/sorting/distinct/Solution.java
@@ -0,0 +1,33 @@
+package com.codility.lessons.sorting.distinct;
+
+import java.util.Arrays;
+
+/**
+ * Solution for Codility lesson:
+ * https://app.codility.com/programmers/lessons/6-sorting/distinct/
+ * 
+ * Idea:
+ */
+public class Solution {
+
+  public static void main(String[] args) {
+  }
+
+  public int solution(int[] A) {
+    if (A.length == 0) {
+      return 0;
+    }
+
+    Arrays.sort(A);
+
+    int distinctValues = 1;
+    for (int i = 1; i < A.length; i++) {
+      if (A[i] != A[i - 1]) {
+        distinctValues++;
+      }
+    }
+
+    return distinctValues;
+  }
+
+}
